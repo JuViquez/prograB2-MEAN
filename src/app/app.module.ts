0ceb17484729fafb09181d585aa457d4daa3db4f
joseant12 <joseantonio122@gmail.com>
2018-02-28 23:25:54 -0600

    Agregado el componente de Instituciones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { InstitucionesComponent } from './instituciones/instituciones.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InstitucionesComponent
   ],
   imports: [
     BrowserModule

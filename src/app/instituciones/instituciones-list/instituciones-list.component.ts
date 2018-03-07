be3afe2ca1aae592d7be51811698fc3cae821ede
joseant12 <joseantonio122@gmail.com>
2018-03-06 23:15:33 -0600

    Se agregó el módulo de escuelas

--- src/app/instituciones/instituciones-list/instituciones-list.component.ts
+++ src/app/instituciones/instituciones-list/instituciones-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Institucion } from '../institucion';
-import { InstitucionesService } from '../instituciones.service';
+import { InstitucionService } from '../institucion.service';
 import { EscuelaService } from '../../escuela/escuela.service';
 import { Escuela } from '../../escuela/escuela';
 
@@ -9,67 +9,92 @@ import { Escuela } from '../../escuela/escuela';
   selector: 'app-instituciones-list',
   templateUrl: './instituciones-list.component.html',
   styleUrls: ['./instituciones-list.component.css'],
-  providers: [InstitucionesService,EscuelaService]
+  providers: [InstitucionService,EscuelaService]
 })
 export class InstitucionesListComponent implements OnInit {
 
   instituciones: Institucion[];
   selectedInstitucion: Institucion;
-  seleccionado: string;
   noInstitucion: Boolean;
-  noSede: Boolean;
+  inputInstitucion: string;
   sedes: any[];
   selectedSede: any;
-  //escuelasService: EscuelaService;
-  escuelas: Escuela[];
+  noSede: Boolean;
+  inputSede: string;
+  escuelas: any[];
+  selectedEscuela: Escuela;
+  noEscuela: Boolean;
+  inputEscuela: string;
 
   selectInstitucion (event: any){
     this.sedes = [];
+    this.escuelas = [];
     if(typeof(this.selectedInstitucion.nombre) == 'undefined'){
       //No encuentra Institución, se habilita la opción de crear una
       this.noInstitucion = true;
       this.noSede=true;
+      this.noEscuela = true;
     }else{
       console.log(this.selectedInstitucion.nombre);
       this.sedes = this.selectedInstitucion.sedes;
       this.noInstitucion = false;
       this.noSede = false;
+      this.noEscuela = false;
     }
   }
-
+  
   selectSede(event: any){
+    this.escuelas = [];
     //this.selectedSede = event.target.value;
     if(typeof(this.selectedSede.nombre) == 'undefined'){
       //No se encuentra sede, se habilita la opción de crear nueva
-      this.noSede = true;     
+      this.noSede = true;  
+      this.noEscuela = true;   
     }
     else{
       this.noSede = false;
-      console.log("-> "+this.selectedSede.id_escuelas);
+      this.noEscuela = false;
       this.getNombreEscuelas(this.selectedSede.id_escuelas);
     }
   }
 
-  getNombreEscuelas(arreglo: string[]){
-    this.escuelasService.getEscuelas(arreglo).then((data: Escuela[]) => { 
-      this.escuelas = data;})
-    console.log("arreglo: "+this.escuelas);
+  selectEscuela(event: any){
+    if(typeof(this.selectedEscuela.nombre) == 'undefined'){
+      //No se encuentra escuela, se habilita la opción de crear nueva
+      this.noEscuela = true;     
+    }
+    else{
+      this.noEscuela = false;
+    }
   }
 
-  constructor(private institucionService: InstitucionesService,private escuelasService: EscuelaService) {}
+  getNombreEscuelas(arreglo: string[]){
+    this.escuelasService.getEscuelas(arreglo).then((data:Escuela[]) => { 
+      this.escuelas = data; console.log("arreglo: "+this.escuelas);})}
+
+  EnviarDocumento(){
+    var InstitucionOutput = new Institucion;
+
+    if(this.noInstitucion){
+      var arr: string[];
+      arr = new Array();
+      arr.push(this.inputEscuela);
+      InstitucionOutput.nombre = this.inputInstitucion;
+      InstitucionOutput.sedes = [{nombre:this.inputSede,id_escuelas:arr}];
+      console.log(InstitucionOutput);
+    }
+  }
+  
+  constructor(private institucionService: InstitucionService,private escuelasService: EscuelaService) {}
 
   ngOnInit() {
     this.sedes = new Array();
     this.noInstitucion = false;
     this.noSede = false;
-    this.institucionService.getUsers().then((data: Institucion[]) => { 
-      this.instituciones = data;
-    })/*
-    this.selectedInstitucion.nombre="UNED";
-    this.selectedInstitucion.sedes.push({nombre:"Guadalupe",id_escuelas:[]});
-    this.institucionService.getUsers().then((data: Institucion[]) => { 
+    this.institucionService.getInstituciones().then((data: Institucion[]) => { 
       this.instituciones = data;
     })
-*/
   }
+
+
 }

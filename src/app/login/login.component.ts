a2aa0368022ba302f96fd0dacdac898634817f9f
joseant12 <joseantonio122@gmail.com>
2018-03-09 23:56:42 -0600

    Agregado el manejo de sesiones, falta el redirect y log out

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'; 
+import { HttpModule } from '@angular/http';
 import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
-import { LoginService } from '../login/login.service';
+import { LoginService } from './login.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [LoginService]
 })
 export class LoginComponent implements OnInit {
   messageClass;
@@ -37,6 +40,7 @@ export class LoginComponent implements OnInit {
     this.form.controls['password'].enable();
   }
 
+
   onLoginSubmit() {
     this.processing = true; 
     this.disableForm(); 
@@ -44,15 +48,19 @@ export class LoginComponent implements OnInit {
       username: this.form.get('username').value,
       password: this.form.get('password').value
     };
-    this.loginservice.login(user).subscribe(data => {
+    this.loginservice.login(user).then((data: any) => {
       if(!data.success){
-
+        console.log("usuario no identificado "+data.message)
+        this.processing = false;
+        this.enableForm();
       }else{
-        
+        this.loginservice.guardarDatos(data.user);
       }
     })
   
   }
+  
+  
 
   ngOnInit() {
   }
--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Http, Headers, RequestOptions } from '@angular/http';
+import { LoginComponent } from './login.component';
+import { Http, RequestOptions, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 
 @Injectable()
@@ -7,7 +8,33 @@ export class LoginService {
 
   constructor( private http: Http ) { }
 
-  login(user) {
-    return this.http.post('/login', user).map(res => res.json());
+  login(user): Promise<void | any> {
+    return this.http.get('/login/'+user.username+'/'+user.password).toPromise().then(response => response.json() as any ).catch(this.handleError);
   }
+
+  guardarDatos(usuario){
+    localStorage.setItem('nombre', usuario.nombre);
+    localStorage.setItem('permiso', usuario.permiso);
+    localStorage.setItem('institucion', usuario.institucion.nombre);
+    localStorage.setItem('sede', usuario.institucion.sede);
+    localStorage.setItem('escuela', usuario.escuela);
+  }
+
+  consultarDatos(){
+    var datos = {
+      nombre: localStorage.getItem('nombre'),
+      institucion: localStorage.getItem('institucion'),
+      sede : localStorage.getItem('sede'),
+      escuela: localStorage.getItem('escuela'),
+      permiso: localStorage.getItem('permiso')
+    }
+    return datos;
+  }
+
+  private handleError (error: any) {
+    let errMsg = (error.message) ? error.message :
+    error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+    console.error(errMsg);
+  }
+
 }

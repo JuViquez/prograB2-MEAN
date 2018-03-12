df26b9a1c15ff0a4e26fd0f3af765a3dc16c098b
JuViquez <jviquez2@gmail.com>
2018-03-12 15:18:01 -0600

    matricula

--- src/app/matricula/matricula/matricula.component.ts
+++ src/app/matricula/matricula/matricula.component.ts
@@ -3,6 +3,8 @@ import { NavigationBarComponent } from '../../navigation/navigation-bar/navigati
 import { LoginService } from '../../login/login.service'
 import { Usuario } from '../../usuario/usuario';
 import { GrupoService } from '../../grupo/grupo.service'
+import { Grupo } from '../../grupo/grupo';
+import { Curso } from '../../models/curso';
 
 @Component({
   selector: 'app-matricula',
@@ -14,18 +16,59 @@ export class MatriculaComponent implements OnInit {
 
   session: Usuario
 
+  cursos: any[];
+  selectedCurso: string;
+
+  grupos: Grupo[];
+  nombresProfesor: string;
+
   constructor(private loginService: LoginService,
-              private grupoService: GrupoService) { }
+              private grupoService: GrupoService) { 
+                this.selectedCurso = "";
+              }
 
   ngOnInit() {
     this.session = this.loginService.consultarDatos();
     var historialLength = this.session.historial_cursos.length;
+    var codigosCurso = new Array();
     for(var i = 0; i < historialLength; i++){
-      
+      if(this.session.historial_cursos[i].estado == 'Aprobado'){
+        codigosCurso.push(this.session.historial_cursos[i])
+      }
     }
-    console.log(this.session);
+    this.grupoService.getGruposNoCursados(codigosCurso).then((grupos: Grupo[]) =>{
+      this.grupos = grupos;
+      this.cursos = this.grupos.map(function(grupo){
+        return grupo.curso;
+      }).filter(function(item,pos,array){
+        return array.indexOf(item) == pos;
+      })
+      
+    });
+   
   }
 
+  matricularClicked(checked: boolean, grupo: Grupo){
+    var putGrupo = grupo;
+    if(checked){
+      var listaEstudiantes = putGrupo.lista_estudiantes.map(function(x){ return x.id_estudiante })
+      putGrupo.lista_estudiantes.splice(listaEstudiantes.indexOf(this.session._id),1);
+    }
+    else{
+      putGrupo.lista_estudiantes.push({id_estudiante: this.session._id, evaluaciones: []});
+    }
+    console.log(putGrupo);
+    this.grupos[this.grupos.indexOf(grupo)] = putGrupo;
+    return !checked;
+  }
+
+  setSelectedCurso(codCurso: string){
+    this.selectedCurso = codCurso;
+  }
+
+  checkGrupo(grupo: Grupo){
+    return grupo.lista_estudiantes.map(function(x){return x.id_estudiante}).indexOf(this.session._id) != -1 && grupo.cupos > 0;
+  }
 
 
 }

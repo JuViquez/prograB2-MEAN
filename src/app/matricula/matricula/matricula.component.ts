a6eadc3906c81024659c83411be444ab8f9c46c2
JuViquez <jviquez2@gmail.com>
2018-03-12 20:58:21 -0600

    update

--- src/app/matricula/matricula/matricula.component.ts
+++ src/app/matricula/matricula/matricula.component.ts
@@ -7,6 +7,8 @@ import { Grupo } from '../../grupo/grupo';
 import { Curso } from '../../models/curso';
 import { InstitucionService } from '../../instituciones/institucion.service'
 import { Institucion } from '../../instituciones/institucion';
+import { ListaEstudiantes } from '../../models/lista-estudiantes';
+import { HistorialCurso } from '../../models/historial-curso';
 
 @Component({
   selector: 'app-matricula',
@@ -18,24 +20,19 @@ export class MatriculaComponent implements OnInit {
 
   session: Usuario
 
-  cursos: any[];
-  selectedCurso: string;
-
   grupos: Grupo[];
-  idsGruposMatriculados: string[];
-  nombresProfesor: string;
+  gruposMatriculados: Grupo[];
 
   currentInstitucion: Institucion;
 
   constructor(private loginService: LoginService,
               private grupoService: GrupoService,
               private institucionService: InstitucionService) { 
-                this.selectedCurso = "";
+                this.gruposMatriculados = [];
               }
 
   ngOnInit() {
     this.session = this.loginService.consultarDatos();
-    console.log(this.session);
     var historialLength = this.session.historial_cursos.length;
     var codigosCurso = new Array();
     for(var i = 0; i < historialLength; i++){
@@ -46,45 +43,51 @@ export class MatriculaComponent implements OnInit {
     this.institucionService.getInstitucionById(this.session.institucion.id_institucion).then((institucion: Institucion) => {
       console.log(institucion);
       this.currentInstitucion = institucion;
-      this.grupoService.getGruposNoCursados(codigosCurso, institucion.periodo ).then((grupos: Grupo[]) =>{
+      this.grupoService.getGruposNoCursados(codigosCurso, institucion.periodo).then((grupos: Grupo[]) =>{
         this.grupos = grupos;
-        this.cursos = this.grupos.map(function(grupo){
-          return grupo.curso;
-        }).filter(function(item,pos,array){
-          return array.indexOf(item) == pos;
-        })
-        
       });
     })
    
    
   }
 
-  matricularClicked(checked: boolean, grupo: Grupo){
-    var putGrupo = grupo;
-    if(checked){
-      var listaEstudiantes = putGrupo.lista_estudiantes.map(function(x){ return x.id_estudiante })
-      putGrupo.lista_estudiantes.splice(listaEstudiantes.indexOf(this.session._id),1);
-      putGrupo.cupos++;
-      this.idsGruposMatriculados.splice(this.idsGruposMatriculados.indexOf(putGrupo._id),1);
+  matricularClicked( grupo: Grupo){
+    if(grupo.cupos > 0 && !this.gruposMatriculados.find(function(x){
+      return x.curso == grupo.curso;
+    }))
+    {
+      var newHistorial = this.iniciarHistorial(grupo);
+      this.session.historial_cursos.push(newHistorial);
+      this.grupos.splice(this.grupos.indexOf(grupo),1);
+      grupo.cupos--;
+      var lista = new ListaEstudiantes();
+      lista.id_estudiante = this.session._id;
+      lista.evaluaciones = [];
+      grupo.lista_estudiantes.push(lista);
+      this.gruposMatriculados.push(grupo);
     }
-    else{
-      putGrupo.lista_estudiantes.push({id_estudiante: this.session._id, evaluaciones: []});
-      putGrupo.cupos--;
-      this.idsGruposMatriculados.push(putGrupo._id);
-    }
-    console.log(putGrupo);
-    this.grupos[this.grupos.indexOf(grupo)] = putGrupo;
-    return !checked;
+  }
+ 
+  desmatricularClicked(grupo: Grupo){
+    var lista = new ListaEstudiantes();
+    this.gruposMatriculados.splice(this.gruposMatriculados.indexOf(grupo),1);
+    lista.id_estudiante = this.session._id;
+    lista.evaluaciones = [];
+    grupo.lista_estudiantes.splice(grupo.lista_estudiantes.indexOf(lista),1);
+    grupo.cupos++;
+    this.grupos.push(grupo);
+    var newHistorial = this.iniciarHistorial(grupo);
+    this.session.historial_cursos.splice(this.session.historial_cursos.indexOf(newHistorial),1);
   }
 
-  setSelectedCurso(codCurso: string){
-    this.selectedCurso = codCurso;
+  private iniciarHistorial(grupo: Grupo){
+    var newHistorial = new HistorialCurso();
+    newHistorial.codigo_curso = grupo.curso.codigo_curso;
+    newHistorial.estado = 'Cursando';
+    newHistorial.id_grupo = grupo._id;
+    newHistorial.nombre = grupo.curso.nombre;
+    newHistorial.nota_final = 0;
+    newHistorial.periodo = grupo.periodo;
+    return newHistorial;
   }
-
-  checkGrupo(grupo: Grupo){
-    return this.idsGruposMatriculados.indexOf(grupo._id) != -1 && grupo.cupos > 0;
-  }
-
-
 }
